70d54158f7297dc9969c6c473b04858721b08feb
Avnerus <avnerus@gmail.com>
2017-11-05 02:28:20 +0200

    don't clear cookie on authentication failure. It's just a failure to connect OAuth

--- src/express/failure-redirect.js
+++ src/express/failure-redirect.js
@@ -5,10 +5,11 @@ export default function failureRedirect (options = {}) {
   debug('Registering failureRedirect middleware');
 
   return function (error, req, res, next) {
+      /*
     if (options.cookie && options.cookie.enabled) {
       debug(`Clearing old '${options.cookie.name}' cookie`);
       res.clearCookie(options.cookie.name);
-    }
+    }*/
 
     if (res.hook && res.hook.data && res.hook.data.__redirect) {
       const { url, status } = res.hook.data.__redirect;
